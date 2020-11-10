03a6863dfdcd60b9abce09d36a8c0ac9a6130eeb
olivgb <52329070+olivgb@users.noreply.github.com>
2020-11-10 10:46:05 -0300

    Adição de abreviação e continuação.
    pronto

---
+++ FormataNome/FormataNome/AbreviarNome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+
+namespace FormataNome
+{
+    public class AbreviarNome
+    {
+        public string abreviandoNome(string nome)
+        {
+
+            string[] n = nome.Split(' ');
+
+            string sigla = "";
+
+            CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
+            TextInfo textInfo = cultureInfo.TextInfo;
+            
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                //Dispenso a palavras menores que 3 caracteres
+                if (n[i].Length > 3)
+                {
+                    //Pego somente a primeira da palavra
+                    sigla = sigla + n[i].Substring(0, 1);
+                }
+            }
+
+            return sigla;
+        }
+    }
+}
--- FormataNome/FormataNome/Program.cs
+++ FormataNome/FormataNome/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
 using System.Threading;
 
 namespace FormataNome
@@ -8,22 +9,49 @@ namespace FormataNome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Insira o nome a ser formatado: ");
-            string nome = Console.ReadLine();
+            string ativo = "s";
 
-            string[] nomes = new string[100];
-            string recebeNome = string.Empty;
-            CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
-            TextInfo textInfo = cultureInfo.TextInfo;
-
-            for (int i = 0; i < nome.Length; i++)
+            while (ativo == "s")
             {
 
-                nomes[i] = Convert.ToString(nome[i]);
-                recebeNome += nomes[i];
+                Console.Write("Insira o nome a ser formatado: ");
+                string nome = Console.ReadLine();
+                
+
+                string[] nomes = new string[100];
+                string recebeNome = string.Empty;
+                CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
+                TextInfo textInfo = cultureInfo.TextInfo;
+
+                for (int i = 0; i < nome.Length; i++)
+                {
+
+                    nomes[i] = Convert.ToString(nome[i]);
+                    recebeNome += nomes[i];
+
+                   
+                }
+                Console.WriteLine(nomes[0]);
+                Console.WriteLine(textInfo.ToTitleCase("Nome Formatado: "+textInfo.ToLower(recebeNome)));
+
+
+                AbreviarNome abreviarNome = new AbreviarNome();
+
+                Console.WriteLine("Nome Abreviado: " + abreviarNome.abreviandoNome(textInfo.ToUpper(recebeNome)));
+
+
+                Console.WriteLine();
+                Console.WriteLine("---------------------------------------------------");
+                Console.WriteLine("          Deseja continuar? (s/n)");
+                ativo = Console.ReadLine();
+                Console.WriteLine("---------------------------------------------------");
+                Console.WriteLine();
+
             }
 
-            Console.WriteLine(textInfo.ToTitleCase(textInfo.ToLower(recebeNome)));
+            Console.WriteLine("bye bye...");
+
+
 
 
             Console.ReadLine();
