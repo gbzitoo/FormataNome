af8241dd4a43ddfd09bba10774cd6243aa2c0e52
olivgb <52329070+olivgb@users.noreply.github.com>
2020-11-10 09:13:56 -0300

    Criado
    criado

---
+++ FormataNome/FormataNome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace FormataNome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o nome a ser formatado: ");
+            string nome = Console.ReadLine();
+
+            string[] nomes = new string[100];
+            string recebeNome = string.Empty;
+            CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
+            TextInfo textInfo = cultureInfo.TextInfo;
+
+            for (int i = 0; i < nome.Length; i++)
+            {
+
+                nomes[i] = Convert.ToString(nome[i]);
+                recebeNome += nomes[i];
+            }
+
+            Console.WriteLine(textInfo.ToTitleCase(textInfo.ToLower(recebeNome)));
+
+
+            Console.ReadLine();
+        }
+    }
+}
